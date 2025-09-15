2ceb36a269e81a6de9e9b86cca7ddd78b4a8aae9
Markus Kalkbrenner <git@kalki.de>
2025-09-15 22:58:33 +0200

    changed frame struct

--- include/serum-version.h
+++ include/serum-version.h
@@ -33,28 +33,38 @@
 // Main data structure that the library returns
 typedef struct
 {
-  uint8_t SerumVersion;
-  uint32_t flags;
-  uint32_t nocolors;
-  uint32_t width32;
-  uint32_t width64;
-  uint32_t frameID;
+  // data for v1 Serum format
+  uint8_t* frame;      // return the colorized frame
+  uint8_t* palette;    // and its palette
+  uint8_t* rotations;  // and its color rotations
+  // data for v2 Serum format
+  // the frame (frame32 or frame64) corresponding to the resolution of the ROM must ALWAYS be defined
+  // if a frame pointer is defined, its width, rotations and rotationsinframe pointers must be defined
+  uint16_t* frame32;
+  uint32_t width32;  // 0 is returned if the 32p colorized frame is not available for this frame
+  uint16_t* rotations32;
+  uint16_t* rotationsinframe32;  // [width32*32*2] precalculated array to tell if a color is in a color rotations of the
+                                 // frame ([X*Y*0]=0xffff if not part of a rotation)
+  uint8_t* modifiedelements32;   // (optional) 32P pixels modified during the last rotation
+  uint16_t* frame64;
+  uint32_t width64;  // 0 is returned if the 64p colorized frame is not available for this frame
+  uint16_t* rotations64;
+  uint16_t* rotationsinframe64;  // [width64*64*2] precalculated array to tell if a color is in a color rotations of the
+                                 // frame ([X*Y*0]=0xffff if not part of a rotation)
+  uint8_t* modifiedelements64;   // (optional) 64P pixels modified during the last rotation
+  // common data
+  uint32_t SerumVersion;  // SERUM_V1 or SERUM_V2
+  /// <summary>
+  /// flags for return:
+  /// if flags & FLAG_RETURNED_32P_FRAME_OK (after Serum_Colorize) : frame32 has been filled
+  /// if flags & FLAG_RETURNED_64P_FRAME_OK (after Serum_Colorize) : frame64 has been filled
+  /// if flags & FLAG_RETURNED_EXTRA_AVAILABLE (after Serum_Load) : at least 1 frame in the file has an extra resolution
+  /// version none of them, display the original frame
+  /// </summary>
+  uint8_t flags;
+  uint32_t nocolors;   // number of shades of orange in the ROM
+  uint32_t ntriggers;  // number of triggers in the Serum file
+  uint32_t triggerID;  // return 0xffff if no trigger for that frame, the ID of the trigger if one is set for that frame
+  uint32_t frameID;    // for CDMD ingame tester
   uint32_t rotationtimer;
-  uint32_t ntriggers;
-  uint32_t triggerID;
-
-  // Frame data pointers
-  uint8_t* frame;      // V1 format frame data
-  uint8_t* palette;    // V1 format palette data
-  uint8_t* rotations;  // V1 format color rotation data
-
-  // V2 format data
-  uint16_t* frame32;             // 32-line high resolution frame
-  uint16_t* frame64;             // 64-line high resolution frame
-  uint16_t* rotations32;         // 32-line color rotations
-  uint16_t* rotations64;         // 64-line color rotations
-  uint16_t* rotationsinframe32;  // 32-line rotation data in frame
-  uint16_t* rotationsinframe64;  // 64-line rotation data in frame
-  uint8_t* modifiedelements32;   // 32-line modified elements
-  uint8_t* modifiedelements64;   // 64-line modified elements
 } Serum_Frame_Struc;
